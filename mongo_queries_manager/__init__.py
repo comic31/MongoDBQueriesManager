0ad6031af7c6812414fcb2390be9ed76a094d66f
danglat <theodangla32@gmail.com>
2021-02-12 21:01:43 +0100

    Refactor errors logic, load news errors (MongoDBQueriesManagerBaseError, SkipError, LimitError,ListOperatorError, FilterError)

--- mongo_queries_manager/__init__.py
+++ mongo_queries_manager/__init__.py
@@ -2,39 +2,50 @@
 # coding: utf-8
 # Copyright (c) Modos Team, 2020
 
-from urllib import parse
 from typing import Dict, Any
+from urllib import parse
 
-from .mongodb_queries_manager import MongoDBQueriesManager, MongoDBQueriesManagerError
+from .mongodb_queries_manager import MongoDBQueriesManager, MongoDBQueriesManagerBaseError, SkipError, LimitError, \
+    ListOperatorError, FilterError
 
 __version__ = "0.0.1"
 
 __all__ = [
     'mqm',
-    'MongoDBQueriesManagerError',
+    'MongoDBQueriesManagerBaseError',
+    'SkipError',
+    'LimitError',
+    'ListOperatorError',
+    'FilterError',
 ]
 
 
 def mqm(string_query: str) -> Dict[str, Any]:
-    try:
-        args = [elem for elem in (parse.unquote(string_query)).split('&')]
+    """ This method convert a string query into a MongoDB query dict.
 
-        mongodb_query = {'filter': {},
-                         'sort': None,
-                         'skip': 0,
-                         'limit': 0,
-                         }
+    Args:
+        string_query:
 
-        for arg in args:
-            if arg.startswith('sort='):
-                mongodb_query['sort'] = MongoDBQueriesManager.sort_logic(sort_params=arg)
-            elif arg.startswith('limit='):
-                mongodb_query['limit'] = MongoDBQueriesManager.limit_logic(limit_param=arg)
-            elif arg.startswith('skip='):
-                mongodb_query['skip'] = MongoDBQueriesManager.skip_logic(skip_param=arg)
-            elif arg != '':
-                mongodb_query['filter'] = {**mongodb_query['filter'],
-                                           **MongoDBQueriesManager.filter_logic(filter_params=arg)}
-    except Exception as err:
-        raise MongoDBQueriesManagerError(raison=str(err))
+    Returns:
+
+    """
+    args = list(parse.unquote(string_query).split('&'))
+
+    mongodb_query: Dict[str, Any] = {'filter': {},
+                                     'sort': None,
+                                     'skip': 0,
+                                     'limit': 0,
+                                     }
+
+    for arg in args:
+        if arg.startswith('sort='):
+            mongodb_query['sort'] = MongoDBQueriesManager.sort_logic(sort_params=arg)
+        elif arg.startswith('limit='):
+            mongodb_query['limit'] = MongoDBQueriesManager.limit_logic(limit_param=arg)
+        elif arg.startswith('skip='):
+            mongodb_query['skip'] = MongoDBQueriesManager.skip_logic(skip_param=arg)
+        elif arg != '':
+            mongodb_query['filter'] = {**mongodb_query['filter'],
+                                       **MongoDBQueriesManager.filter_logic(filter_params=arg)}
+
     return mongodb_query
