89a13bb1c93250a37a8671a3f05391c13863aa52
danglat <theodangla32@gmail.com>
2021-02-12 14:44:07 +0100

    Create setup Pyfile

---
+++ setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup, find_packages
+
+PATH = pathlib.Path(__file__).parent
+
+README = (PATH / "README.md").read_text()
+
+setup(
+    name='MongoDBQueriesManager',
+    version='0.0.1',
+    packages=find_packages(exclude=("tests", "examples")),
+    url='https://github.com/comic31/MongoDBQueriesManager',
+    license='MIT',
+    author='Modo team',
+    author_email='theodangla32@gmail.com',
+    include_package_data=True,
+    description='Convert query parameters from API urls to MongoDB queries !',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'dateparser>=1.0.0',
+        'pymongo>=3.11.3'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Topic :: Database',
+        'Topic :: System :: Networking',
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+)
