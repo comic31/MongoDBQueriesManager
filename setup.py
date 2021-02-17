9126d84a95e0c0b3e775e0fc9176d2bac778bfc6
danglat <theodangla32@gmail.com>
2021-02-17 18:29:05 +0100

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mongo-queries-manager',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=("tests", "examples")),
     url='https://github.com/comic31/MongoDBQueriesManager',
     license='MIT',
