4630d84e873d4117f1d9ba7f9561769a86ab21ee
danglat <theodangla32@gmail.com>
2021-02-17 17:48:28 +0100

    Create test custom cast

---
+++ tests/test_custom_cast.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) Modos Team, 2020
+
+from typing import List
+
+import pytest
+
+from mongo_queries_manager import mqm, CustomCasterFail
+
+
+class TestCustomCast:
+    # Test good custom type
+    def test_custom_cast(self):
+        query_result = mqm(string_query="price=string(5)", casters={'string': str})
+
+        assert query_result == {'filter': {'price': '5'}, 'sort': None, 'skip': 0, 'limit': 0}
+
+    def test_custom_cast_2(self):
+
+        def parse_custom_list(custom_list: str) -> List[str]:
+            return custom_list.split(';')
+
+        query_result = mqm(string_query="price=string(5)&name=John&in_stock=custom_list(1;2;3;4)",
+                           casters={'string': str, 'custom_list': parse_custom_list})
+
+        assert query_result == {'filter': {'price': '5', 'name': 'John', 'in_stock': {'$in': ['1', '2', '3', '4']}},
+                                'sort': None, 'skip': 0, 'limit': 0}
+
+    # Test bad custom type
+    def test_custom_cast_fail(self):
+
+        with pytest.raises(CustomCasterFail) as excinfo:
+            query_result = mqm(string_query="price=float(A.B)", casters={'float': float})
+
+        assert excinfo.value.__str__() == 'Fail to cast float(A.B) with caster float'
