bd0f455eb3327306cbc4cfece541affb3f3ac09b
danglat <theodangla32@gmail.com>
2021-02-26 11:27:01 +0100

    Bump to 0.1.4

--- mongo_queries_manager/__init__.py
+++ mongo_queries_manager/__init__.py
@@ -8,7 +8,7 @@ from urllib import parse
 from .mongodb_queries_manager import MongoDBQueriesManager, MongoDBQueriesManagerBaseError, SkipError, LimitError, \
     ListOperatorError, FilterError, CustomCasterFail
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     'mqm',
