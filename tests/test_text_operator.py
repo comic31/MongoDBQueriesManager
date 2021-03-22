ecae40ec457ef2d9684632eb40a938adaaf89a69
danglat <theodangla32@gmail.com>
2021-03-22 22:17:50 +0100

    Refactor text operator tests

--- tests/test_text_operator.py
+++ tests/test_text_operator.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 # Copyright (c) Modos Team, 2020
 
-import re
-
 import pytest
 
 from mongo_queries_manager import mqm, TextOperatorError
@@ -14,13 +12,12 @@ class TestTextOperator:
     def test_good_text_operator(self):
         query_result = mqm(string_query="$text=toto")
 
-        assert query_result == {'filter': {'$text': {"$search": 'toto'}}, 'sort': None, 'skip': 0, 'limit': 0}
+        assert query_result == {'filter': {'$text': {'$search': 'toto'}}, 'sort': None, 'skip': 0, 'limit': 0}
 
-    def test_good_text_operator_regex(self):
-        query_result = mqm(string_query="$text=/^06/")
+    def test_good_text_operator2(self):
+        query_result = mqm(string_query='$text="toto"')
 
-        assert query_result == {'filter': {'$text': {"$search": re.compile(r'/^06/')}}, 'sort': None,
-                                'skip': 0, 'limit': 0}
+        assert query_result == {'filter': {'$text': {'$search': '"toto"'}}, 'sort': None, 'skip': 0, 'limit': 0}
 
     # Skip bad tests part
     def test_empty_test_operator(self):
