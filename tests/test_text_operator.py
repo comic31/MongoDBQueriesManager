ca6a4f6dd46ec8f5dbbb78f09153dedca7cf7d74
danglat <theodangla32@gmail.com>
2021-03-22 21:58:15 +0100

    Create test text operator

---
+++ tests/test_text_operator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright (c) Modos Team, 2020
+
+import re
+
+import pytest
+
+from mongo_queries_manager import mqm, TextOperatorError
+
+
+class TestTextOperator:
+    # Text operator good tests part
+    def test_good_text_operator(self):
+        query_result = mqm(string_query="$text=toto")
+
+        assert query_result == {'filter': {'$text': {"$search": 'toto'}}, 'sort': None, 'skip': 0, 'limit': 0}
+
+    def test_good_text_operator_regex(self):
+        query_result = mqm(string_query="$text=/^06/")
+
+        assert query_result == {'filter': {'$text': {"$search": re.compile(r'/^06/')}}, 'sort': None,
+                                'skip': 0, 'limit': 0}
+
+    # Skip bad tests part
+    def test_empty_test_operator(self):
+        with pytest.raises(TextOperatorError) as excinfo:
+            query_result = mqm(string_query="$text=")
+
+        assert excinfo.value.__str__() == 'Bad $text value'
