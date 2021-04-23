9b0eae0aa761ed4145b9f5dd5ae294a2fb452602
danglat <theodangla32@gmail.com>
2021-04-23 17:33:59 +0200

    Bump versions

--- mongo_queries_manager/__init__.py
+++ mongo_queries_manager/__init__.py
@@ -8,7 +8,7 @@ from urllib import parse
 from .mongodb_queries_manager import MongoDBQueriesManager, MongoDBQueriesManagerBaseError, SkipError, LimitError, \
     ListOperatorError, FilterError, CustomCasterFail, TextOperatorError, ProjectionError
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __all__ = [
     'mqm',
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mongo-queries-manager',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(exclude=("tests", "examples")),
     url='https://github.com/comic31/MongoDBQueriesManager',
     license='MIT',
