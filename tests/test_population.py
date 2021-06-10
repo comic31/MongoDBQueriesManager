60c11d12098d2b14329828552db7b01d952f5276
danglat <theodangla32@gmail.com>
2021-06-10 09:47:41 +0200

    Add more population tests

--- tests/test_population.py
+++ tests/test_population.py
@@ -4,7 +4,7 @@
 
 import pytest
 
-from mongo_queries_manager import mqm
+from mongo_queries_manager import mqm, LogicalPopulationError, LogicalSubPopulationError
 
 
 class TestPopulation:
@@ -26,6 +26,115 @@ class TestPopulation:
                                 'projection': None, 'population': [{'path': 'user', 'projection': None},
                                                                    {'path': 'settings', 'projection': None}]}
 
+    def test_multi_population_2(self):
+        query_result = mqm(string_query="populate=user,user.settings", populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': None, 'population': [{'path': 'user', 'projection': None,
+                                                                    'population': [{
+                                                                        'path': 'settings',
+                                                                        'projection': None}]}]}
+
+    def test_multi_population_3(self):
+        query_result = mqm(string_query="populate=user,user.settings,user.comments,user.info", populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': None, 'population': [{'path': 'user', 'projection': None,
+                                                                    'population': [{
+                                                                        'path': 'settings',
+                                                                        'projection': None}, {
+                                                                        'path': 'comments',
+                                                                        'projection': None},
+                                                                        {
+                                                                            'path': 'info',
+                                                                            'projection': None}
+                                                                    ]}]}
+
+    def test_multi_population_4(self):
+        query_result = mqm(string_query="populate=user,user.settings,user.settings.info,user.settings.info.rates",
+                           populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': None,
+                                'population': [
+                                    {'path': 'user',
+                                     'projection': None,
+                                     'population': [
+                                         {
+                                             'path': 'settings',
+                                             'projection': None,
+                                             'population': [
+                                                 {
+                                                     'path': 'info',
+                                                     'projection': None,
+                                                     'population': [
+                                                         {
+                                                             'path': 'rates',
+                                                             'projection': None
+                                                         }]
+                                                 }]
+                                         }]
+                                     }
+                                ]}
+
+    def test_multi_population_5(self):
+        query_result = mqm(
+            string_query="populate=user,user.settings,user.settings.notifications,user.settings.configuration",
+            populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': None, 'population': [
+                {'path': 'user',
+                 'projection': None,
+                 'population': [
+                     {
+                         'path': 'settings',
+                         'projection': None,
+                         'population': [
+                             {
+                                 'path': 'notifications',
+                                 'projection': None
+                             },
+                             {
+                                 'path': 'configuration',
+                                 'projection': None
+                             }
+                         ]
+                     }
+                 ]}]}
+
+    def test_multi_population_6(self):
+        query_result = mqm(
+            string_query="populate=user,user.life,user.life.info,user.settings,user.settings.notifications",
+            populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': None, 'population': [
+                {'path': 'user',
+                 'projection': None,
+                 'population': [
+                     {
+                         'path': 'life',
+                         'projection': None,
+                         'population': [
+                             {
+                                 'path': 'info',
+                                 'projection': None
+                             }
+                         ]
+                     },
+                     {
+                         'path': 'settings',
+                         'projection': None,
+                         'population': [
+                             {
+                                 'path': 'notifications',
+                                 'projection': None
+                             }
+                         ]
+                     }
+                 ]}]}
+
     def test_simple_population_with_projection(self):
         query_result = mqm(string_query="fields=-created_at,-updated_at,hives.label&populate=hives", populate=True)
 
@@ -41,3 +150,58 @@ class TestPopulation:
                                 'projection': {'created_at': 0, 'updated_at': 0},
                                 'population': [{'path': 'hives', 'projection': {'label': 1, '_id': 1}},
                                                {'path': 'data', 'projection': {'temperature': 1}}]}
+
+    def test_multi_population_with_multi_projection_2(self):
+        query_result = mqm(string_query="fields=-created_at,-updated_at,-service.created_at,-service.updated_at,"
+                                        "-service.description.created_at,-service."
+                                        "description.updated_at&populate=service,service.description,service."
+                                        "description.picture,animal,animal.info", populate=True)
+
+        assert query_result == {'filter': {}, 'sort': None, 'skip': 0, 'limit': 0,
+                                'projection': {'created_at': 0, 'updated_at': 0},
+                                'population': [{
+                                    'path': 'service',
+                                    'projection': {'created_at': 0, 'updated_at': 0},
+                                    'population': [{
+                                        'path': 'description',
+                                        'projection': {'created_at': 0, 'updated_at': 0},
+                                        'population': [
+                                            {'path': 'picture', 'projection': None}]
+                                    }]},
+                                    {
+                                        'path': 'animal',
+                                        'projection': None,
+                                        'population': [
+                                            {'path': 'info', 'projection': None}]
+                                    }]
+                                }
+
+    def test_bad_population_logic(self):
+        with pytest.raises(LogicalPopulationError) as excinfo:
+            query_result = mqm(string_query="populate=service.description", populate=True)
+
+        assert excinfo.value.__str__() == 'Fail to find logical population item'
+
+    def test_bad_population_logic_2(self):
+        with pytest.raises(LogicalPopulationError) as excinfo:
+            query_result = mqm(string_query="populate=service,service.description,service.description.toto.titi",
+                               populate=True)
+
+        assert excinfo.value.__str__() == 'Fail to find logical population item'
+
+    def test_bad_sub_population_logic(self):
+        with pytest.raises(LogicalSubPopulationError) as excinfo:
+            query_result = mqm(string_query="populate=service,service.description,"
+                                            "service.description.info,service.descriptions.info",
+                               populate=True)
+
+        assert excinfo.value.__str__() == 'Fail to find logical sub population item'
+
+    def test_bad_sub_population_logic_2(self):
+        with pytest.raises(LogicalSubPopulationError) as excinfo:
+            query_result = mqm(string_query="populate=service,service.description,service.description.info,"
+                                            "service.description.info,service.description.info.toto,"
+                                            "service.descriptions.info.titi",
+                               populate=True)
+
+        assert excinfo.value.__str__() == 'Fail to find logical sub population item'
